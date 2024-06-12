5feeb3d5e4579e6bca2b4e9700711e91b255b1a8
Daniel Rodriguez M <daniel.rodriguez182@gmail.com>
2024-06-12 18:35:56 -0500

    Updated route of sw

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
+const url = window.location.href;
+let swLocation = '/telegrom-pwa/sw.js';
+
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw.js');
+  if (url.includes('localhost')) {
+    swLocation = '/sw.js';
+  }
+  navigator.serviceWorker.register('sw.js');
 }
 
 // Referencias de jQuery
